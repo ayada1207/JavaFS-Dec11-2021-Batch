57cee8339b888b2aaacb7523e9cc16e45401c7b9
ayada1997 <arpiit12071997@gmail.com>
2021-12-13 23:06:55 +0530

    incre-Decrement

---
+++ incrementDecrement/src/incrementDecrement/main.java
@@ -0,0 +1,30 @@
+package incrementDecrement;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b = a--;
+		System.out.println(a);
+		System.out.println(b);
+		
+		int i = 10;
+		int j = i++;
+		System.out.println(i);
+		System.out.println(j);
+		
+		int k = 8;
+		int l = --k;
+		System.out.println(k);
+		System.out.println(l);
+
+		int m = 7;
+		int n = ++m;
+		System.out.println(m);
+		System.out.println(n);
+
+
+	}
+
+}
